76a3eb7720d38ec2b76eda53369244364c713599
Architaa-P <architaprakash779@gmail.com>
2024-07-07 11:02:15 +0530

    Create package.py

---
+++ package.py
@@ -0,0 +1,13 @@
+import json
+import subprocess
+from urllib.request import urlopen
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os 
+import smtplib
+import pyjokes
+import operator
+import logging
